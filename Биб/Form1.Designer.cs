ca53bafe75038575433568602130769cd98ffc3e
Annnna ShCh <shchitilina@mail.ru>
2024-02-11 14:12:10 +0300

    Добавьте файлы проекта.

---
+++ Биб/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Биб
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button1.Location = new System.Drawing.Point(45, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Узнать разницу";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(12, 66);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button2.Location = new System.Drawing.Point(312, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(192, 52);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Узнать Високосный год или нет";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button3.Location = new System.Drawing.Point(27, 231);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 41);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Узнать период  суток";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.monthCalendar1.Location = new System.Drawing.Point(312, 12);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 8;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(45, 197);
+            this.maskedTextBox1.Mask = "00:00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 22);
+            this.maskedTextBox1.TabIndex = 9;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(549, 24);
+            this.maskedTextBox2.Mask = "00/00/0000 90:00";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(140, 22);
+            this.maskedTextBox2.TabIndex = 10;
+            this.maskedTextBox2.ValidatingType = typeof(System.DateTime);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button4.Location = new System.Drawing.Point(549, 55);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 49);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Форматировать дату";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(728, 328);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Биб/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DDL;
+namespace Биб
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            DateTime date1 = dateTimePicker1.Value;
+            DateTime date2 = dateTimePicker2.Value;
+
+            TimeSpan difference = DDL.Class1.GetDateDifference(date1, date2);
+
+            MessageBox.Show("Разница между датами: " + difference.TotalDays + " дней", "Результат");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime selectedDate = monthCalendar1.SelectionStart;
+
+            // Проверяем, является ли год високосным
+            bool isLeapYear = Class1.IsLeapYear(selectedDate.Year);
+
+            // Выводим соответствующее сообщение
+            if (isLeapYear)
+            {
+                MessageBox.Show(selectedDate.Year + " - високосный год", "Результат");
+            }
+            else
+            {
+                MessageBox.Show(selectedDate.Year + " - не високосный год", "Результат");
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string timeString = maskedTextBox1.Text;
+
+            // Парсим строку времени в объект DateTime
+            DateTime time;
+            if (!DateTime.TryParseExact(timeString, "HH:mm", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out time))
+            {
+                MessageBox.Show("Некорректный формат времени. Введите время в формате HH:mm.", "Ошибка");
+                return;
+            }
+
+            // Определяем время суток
+            string timeOfDay = Class1.GetTimeOfDay(time);
+
+            // Выводим результат
+            MessageBox.Show("Время суток: " + timeOfDay, "Результат");
+        }
+
+        private void dateTimePicker3_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string dateTimeString = maskedTextBox2.Text;
+
+            // Парсим строку даты и времени в объект DateTime
+            DateTime dateTime;
+            if (!DateTime.TryParseExact(dateTimeString, "dd.MM.yyyy HH:mm", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out dateTime))
+            {
+                MessageBox.Show("Некорректный формат даты и времени. Введите дату и время в формате dd.MM.yyyy HH:mm.", "Ошибка");
+                return;
+            }
+
+            // Форматируем дату и время
+            string formattedDateTime = Class1.FormatDateTime(dateTime, "dd день MM месяц yyyy год  HH часов mm минут ");
+
+            // Выводим отформатированную дату и время
+            MessageBox.Show("Отформатированная дата и время: " + formattedDateTime, "Результат");
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+           
+        }
+    }
+    }
+    
+
